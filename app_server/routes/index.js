680e343a26f8cd3c1c8f9aa091fd567cbc5c1981
Manognacg <84891248+Manognacg@users.noreply.github.com>
2023-05-19 09:15:10 +0530

    Add files via upload

---
+++ app_server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+//var ctrlMain=require('../controllers/main.js')
+const ctrlLocations=require('../controllers/locations');
+const ctrlOthers=require('../controllers/others');
+/* GET home page. */
+//router.get('/',ctrlMain.index);
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+router.get('/about',ctrlOthers.about);
+
+module.exports = router;
